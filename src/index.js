3d9ab7149790bc72f53a0f0fa888a8d8fc49244b
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-23 21:08:54 -0700

    update logic

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ function binarySearch(arr, val) {
   let left = 0;
   let right = arr.length - 1;
   let middle = Math.floor((left + right) / 2);
-  while (arr[middle] !== val) {
-    } else if (arr[middle] > val) {
-      middle - 1 = right;
-    } else {
-      middle = left + 1;
+  while (arr[middle] !== val) {}
+  if (arr[middle] > val) {
+    right = middle - 1;
+  } else {
+    left = middle + 1;
   }
   return -1;
 }
