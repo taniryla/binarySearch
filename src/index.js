2606afb948a0a8550449d21791c0044c18d8f1a9
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-23 21:10:16 -0700

    bracket correction

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ function binarySearch(arr, val) {
   let left = 0;
   let right = arr.length - 1;
   let middle = Math.floor((left + right) / 2);
-  while (arr[middle] !== val) {}
-  if (arr[middle] > val) {
-    right = middle - 1;
-  } else {
-    left = middle + 1;
+  while (arr[middle] !== val) {
+    if (arr[middle] > val) {
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
   }
   return -1;
 }
