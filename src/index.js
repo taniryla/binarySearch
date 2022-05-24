7b4dcb028245972f83c9a15401180a854e819913
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-23 20:53:05 -0700

    move left and right pointers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 function binarySearch(arr, val) {
   let left = 0;
   let right = arr.length - 1;
-  while (arr[left] < arr[right]) {
-    let middle = Math.floor(arr[left] + arr[right]) / 2;
+  while (left < right) {
+    let middle = Math.floor(arr[left] + arr[right]) / 2 - 1;
     if (arr[middle] === val) {
       return middle;
     } else if (arr[middle] > val) {
+      middle = right;
       left++;
     } else if (arr[middle] < val) {
+      middle = left;
       right++;
     } else {
       return -1;
