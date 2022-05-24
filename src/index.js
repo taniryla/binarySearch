07e905bdf5626603869eb32a1e3df1f9fb73d5d4
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-23 20:45:41 -0700

    update control flow logic

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ function binarySearch(arr, val) {
   let left = 0;
   let right = arr.length - 1;
   while (arr[left] < arr[right]) {
-    let middle = (arr[left] + arr[right]) / 2;
-    if (middle === val) {
+    let middle = Math.floor(arr[left] + arr[right]) / 2;
+    if (arr[middle] === val) {
       return middle;
-    } else if (middle > val) {
+    } else if (arr[middle] > val) {
       left++;
-    } else if (middle < val) {
+    } else if (arr[middle] < val) {
       right++;
     } else {
       return -1;
